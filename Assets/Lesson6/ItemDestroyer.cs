7a14d5ad9c0aa588f2106cf1fe6e07c993fc63e7
kanisan123 <30352927+kanisan123@users.noreply.github.com>
2021-04-12 12:02:45 +0900

    画面外に出たオブジェクトを削除するように

---
+++ Assets/Lesson6/ItemDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CarTag") || other.CompareTag("TrafficConeTag") || other.CompareTag("CoinTag"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
